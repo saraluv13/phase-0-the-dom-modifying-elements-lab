16b6a9b0412e24efe155c4e7b9c36db5522fbfc0
Supaasara Bowman <supaasarabowman@Supaasaras-MacBook-Pro.local>
2022-02-13 23:57:10 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('H1');
+
+newHeader.id = ('victory')
+newHeader.id = ('victory')
+
+const main = document.getElementById('victory');
+
+newHeader.id = ('victory')
